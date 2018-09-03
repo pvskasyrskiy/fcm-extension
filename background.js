f407030beea39e752d65609d34a2962eb24a6f3f
Pavel Skasyrskii <Pavel Skasyrskii>
2018-09-03 12:31:23 +0700

    first commit

---
+++ background.js
@@ -0,0 +1,24 @@
+
+/*
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAB1ZCMpMhAXCR9ICckIkHftTmvR4pSf_8",
+  messagingSenderId: "152016073203"
+};
+firebase.initializeApp(config);
+
+function initApp() {
+  // Listen for auth state changes.
+  firebase.auth().onAuthStateChanged(function(user) {
+    console.log('User state change detected from the Background script of the Chrome Extension:', user);
+    // getPermission();
+  });
+
+}
+
+
+window.onload = function() {
+  initApp();
+};
+
+*/
